1a6711dcb85f770bbec86d989a6b454e28e545aa
Philip Zhang <zhangxilai@doublechaintech.com>
2025-03-24 17:56:54 +0800

    🚀change to rest controller

--- src/main/java/com/yourcompany/logservice/EnsureModelController.java
+++ src/main/java/com/yourcompany/logservice/EnsureModelController.java
@@ -7,29 +7,28 @@ import io.teaql.data.meta.EntityMetaFactory;
 import io.teaql.data.sql.SQLRepositorySchemaHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class EnsureModelController {
     @Autowired
     private EntityMetaFactory factory;
 
     @GetMapping(value = "/version/", produces = MediaType.TEXT_PLAIN_VALUE)
-    @ResponseBody
     public String version(){
         return "V1.0.0";
     }
+
     @GetMapping(value = "/contextInfo/", produces = MediaType.TEXT_PLAIN_VALUE)
-    @ResponseBody
     public String contextInfo(@TQLContext UserContext ctx){
         return "your ip: "+ ctx.getClientIp() +"\n" +
                 "from local: " + ctx.isFromLocalhost() ;
     }
 
     @GetMapping("/ensureTables")
-    @ResponseBody
     public Object ensureTable(@TQLContext UserContext context) {
         if(!context.isFromLocalhost()){
             return "ONLY allowed from localhost to protect the import assets!";
@@ -41,9 +40,5 @@ public class EnsureModelController {
             return MapUtil.of("fail", e.getMessage());
         }
     }
-
-
-
-
 
 }
--- src/main/java/com/yourcompany/logservice/SampleController.java
+++ src/main/java/com/yourcompany/logservice/SampleController.java
@@ -4,22 +4,21 @@ import com.yourcompany.logservice.platform.Platform;
 import io.teaql.data.TQLContext;
 import io.teaql.data.UserContext;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class SampleControler {
+@RestController
+public class SampleController {
+
     @GetMapping(value = "/platformName/", produces = MediaType.TEXT_PLAIN_VALUE)
-    @ResponseBody
     public String platformName(@TQLContext UserContext ctx){
         return Q.platforms()
                 .rawSql("select 1 as id, to_char(1+1, '9') as name")
                 .selectName("concat(name,'-add-a-suffix')")
                 .execute(ctx).getName();
     }
+
     @GetMapping(value = "/platform/", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public Platform platform(@TQLContext UserContext ctx){
         return Q.platforms()
                 //.rawSql("select 1 as id, to_char(1+1, '9') as name")
